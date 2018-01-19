49b7894d32ab365f6f87e22c8ea3e620ff3d7386
shkyung <shkyung@hancom.com>
2018-01-19 17:54:56 +0900

    click evt bind

--- src/js/animate.js
+++ src/js/animate.js
@@ -1,3 +1,36 @@
-/**
- * Created by owner on 2018-01-16.
- */
+(function () {
+  $(document).ready(function () {
+    initialize();
+  });
+
+  var $animationViewEl = $();
+
+  function initialize() {
+    $animationViewEl = $(".executeSection");
+
+    registerEvt();
+  }
+
+  function registerEvt() {
+    $("button").click(function (e) {
+      var animationType = $(e.currentTarget).attr("value"),
+      $shapeSvgEl = $animationViewEl.children("svg").children("circle");
+
+      $shapeSvgEl.removeClass();
+
+      if (animationType === "1") {
+        $animationViewEl.hide();
+
+        setTimeout(function() {
+          $animationViewEl.show();
+        }, 100)
+      } else if (animationType === "2") {
+        $shapeSvgEl.addClass("ani2");
+      } else if (animationType === "3") {
+        $shapeSvgEl.addClass("ani3");
+      } else if (animationType === "4") {
+        $shapeSvgEl.addClass("ani4");
+      }
+    })
+  }
+})();
